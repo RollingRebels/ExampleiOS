29e688de3c4fb4d8f393386d8af87a044cb8f2fb
RollingRebels <s.chakraborty23@gmail.com>
2022-06-01 19:43:12 +0530

    Complete feature-1

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var greetingLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func helloButton_Clicked(_ sender: UIButton) {
+        greetingLabel.text = "Hello \(nameTextField.text ?? "")"
+    }
+    
 }
 
